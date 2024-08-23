0fdda15026b4d925ef403f3bb045bf1b38fec639
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-23 12:01:01 +0530

    optimize code

--- src/main/java/com/rkisuru/food/recipe/repository/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rkisuru.food.recipe.repository;
-
-import com.rkisuru.food.recipe.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
