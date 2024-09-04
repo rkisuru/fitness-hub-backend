6cc0179d232f2eaa060555d51c59423d6ecf7bb5
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-04 14:25:43 +0530

    implement ReadFile service

---
+++ src/main/java/com/rkisuru/fitnesshub/service/ReadFile.java
@@ -0,0 +1,29 @@
+package com.rkisuru.fitnesshub.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+@Slf4j
+public class ReadFile {
+
+    public static byte[] readFile(String fileUrl) {
+
+        if (StringUtils.isBlank(fileUrl)) {
+            return null;
+        }
+        try {
+            Path filePath = new File(fileUrl).toPath();
+            return Files.readAllBytes(filePath);
+        } catch (IOException e) {
+            log.warn("No file found at {}", fileUrl);
+        }
+        return null;
+    }
+}
