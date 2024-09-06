88ef30b4e1e4eaf0db977beae9dd0871c92f6bdf
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-06 19:23:55 +0530

    implement exercise service

--- src/main/java/com/rkisuru/fitnesshub/service/ExerciseService.java
+++ src/main/java/com/rkisuru/fitnesshub/service/ExerciseService.java
@@ -2,6 +2,7 @@ package com.rkisuru.fitnesshub.service;
 
 import com.rkisuru.fitnesshub.dto.ExerciseEditRequest;
 import com.rkisuru.fitnesshub.dto.ExerciseRequest;
+import com.rkisuru.fitnesshub.dto.ExerciseResponse;
 import com.rkisuru.fitnesshub.entity.Exercise;
 import com.rkisuru.fitnesshub.entity.Workout;
 import com.rkisuru.fitnesshub.mapper.DtoMapper;
@@ -12,6 +13,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +25,7 @@ public class ExerciseService {
     private final ExerciseRepository exerciseRepository;
     private final DtoMapper mapper;
     private final WorkoutRepository workoutRepository;
+    private final ImageUploadService imageUploadService;
 
     public Long addExercise(ExerciseRequest request, Long workoutId, Authentication connectedUser) {
 
@@ -35,30 +41,25 @@ public class ExerciseService {
         throw new AccessDeniedException("Access denied");
     }
 
-    public String removeExercise(Long exerciseId, Authentication connectedUser, Long workoutId) {
-
-        Workout workout = workoutRepository.findById(workoutId)
-                .orElseThrow(()-> new EntityNotFoundException("Workout not found"));
+    public String removeExercise(Long exerciseId, Authentication connectedUser) {
 
         Exercise exercise = exerciseRepository.findById(exerciseId)
                 .orElseThrow(()-> new EntityNotFoundException("Exercise not found"));
 
-        if (workout.getCreatedBy().equals(connectedUser.getName())) {
+        if (exercise.getCreatedBy().equals(connectedUser.getName())) {
             exerciseRepository.delete(exercise);
             return "Exercise removed Successfully";
         }
         throw new AccessDeniedException("Access denied");
     }
 
-    public Exercise editExercise(Long exerciseId, Authentication connectedUser, Long workoutId, ExerciseEditRequest request) {
+    public Exercise editExercise(Long exerciseId, Authentication connectedUser, ExerciseEditRequest request) {
 
-        Workout workout = workoutRepository.findById(workoutId)
-                .orElseThrow(()-> new EntityNotFoundException("Workout not found"));
 
         Exercise exercise = exerciseRepository.findById(exerciseId)
                 .orElseThrow(()-> new EntityNotFoundException("Exercise not found"));
 
-        if (workout.getCreatedBy().equals(connectedUser.getName())) {
+        if (exercise.getCreatedBy().equals(connectedUser.getName())) {
 
             if (!request.name().isEmpty()) {
                 exercise.setName(request.name());
@@ -70,6 +71,32 @@ public class ExerciseService {
                 exercise.setTargetMuscle(request.targetMuscle());
             }
             return exerciseRepository.save(exercise);
+        }
+        throw new AccessDeniedException("Access denied");
+    }
+
+    public List<ExerciseResponse> getExercisesByWorkoutId(Long workoutId) {
+
+        Workout workout = workoutRepository.findById(workoutId)
+                .orElseThrow(()-> new EntityNotFoundException("Workout not found"));
+
+        return workout.getExercises()
+                .stream()
+                .map(mapper::fromExercise)
+                .toList();
+    }
+
+    public String uploadImage(Long exerciseId, MultipartFile file, Authentication connectedUser) throws IOException {
+
+        Exercise exercise = exerciseRepository.findById(exerciseId)
+                .orElseThrow(()-> new EntityNotFoundException("Exercise not found"));
+
+        if (exercise.getCreatedBy().equals(connectedUser.getName())) {
+
+            var image = imageUploadService.uploadFile(file);
+            exercise.setImage(image);
+            exerciseRepository.save(exercise);
+            return "Image uploaded successfully";
         }
         throw new AccessDeniedException("Access denied");
     }
