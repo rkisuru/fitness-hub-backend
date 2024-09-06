c89c0601cd0b4a05af815467ac1488d1df2ee588
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-06 19:24:46 +0530

    implement Like entity

---
+++ src/main/java/com/rkisuru/fitnesshub/entity/Like.java
@@ -0,0 +1,27 @@
+package com.rkisuru.fitnesshub.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name ="workout_id")
+    private Workout workout;
+}
