0ebb3adf344eb2df5b4df5658f3b4c70dc6726a4
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-06 19:19:27 +0530

    deleted

--- src/main/java/com/rkisuru/fitnesshub/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rkisuru.fitnesshub.repository;
-
-import com.rkisuru.fitnesshub.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
