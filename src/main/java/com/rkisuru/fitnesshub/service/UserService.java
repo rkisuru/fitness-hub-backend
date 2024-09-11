ddcde566c57da8f7446bb541ed304929efc27e01
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-11 12:14:23 +0530

    user service implementation

--- src/main/java/com/rkisuru/fitnesshub/service/UserService.java
+++ src/main/java/com/rkisuru/fitnesshub/service/UserService.java
@@ -1,30 +1,10 @@
 package com.rkisuru.fitnesshub.service;
 
 import com.rkisuru.fitnesshub.entity.User;
-import com.rkisuru.fitnesshub.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
-
-    public UserDetailsService userDetailsService(){
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return userRepository.findByEmail(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-            }
-        };
-    }
-
-    public User findUserById(Long userId) throws Exception{
-        return userRepository.findById(userId).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    }
+    UserDetailsService userDetailsService();
+    public User findUserById(Long userId) throws Exception;
 }
