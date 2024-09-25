bad1006c79e0f40b38bc561803e35c23632611f9
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-25 12:37:48 +0530

    implement error response

---
+++ src/main/java/com/rkisuru/fitnesshub/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.rkisuru.fitnesshub.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ErrorResponse {
+
+    private String message;
+    private int code;
+
+}
