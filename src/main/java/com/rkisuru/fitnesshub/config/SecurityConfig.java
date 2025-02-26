11ac5ccb29b5e0808768a8da4bb3ffc2d330c829
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-26 23:45:09 +0530

    change public auth endpoint

--- src/main/java/com/rkisuru/fitnesshub/config/SecurityConfig.java
+++ src/main/java/com/rkisuru/fitnesshub/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers(
-                                "/api/auth/**",
+                                "/api/v1/auth/**",
                                 "/v2/api-docs",
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
