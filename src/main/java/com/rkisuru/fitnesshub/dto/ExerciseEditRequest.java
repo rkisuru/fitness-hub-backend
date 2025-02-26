9db7853f5489aa9cfc4f192f30bbde76ccdfe49d
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-26 15:42:35 +0530

    removed

--- src/main/java/com/rkisuru/fitnesshub/dto/ExerciseEditRequest.java
+++
@@ -1,9 +0,0 @@
-package com.rkisuru.fitnesshub.dto;
-
-public record ExerciseEditRequest(
-
-        String name,
-        String targetMuscle,
-        String description
-) {
-}
--- src/main/java/com/rkisuru/fitnesshub/dto/FeedbackEditRequest.java
+++
@@ -1,7 +0,0 @@
-package com.rkisuru.fitnesshub.dto;
-
-public record FeedbackEditRequest(
-
-        String feedback
-) {
-}
